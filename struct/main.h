693c3aae7e00fe8a077522817ba99221d5a37e36
JohnAtreides <43500984+JohnAtreides@users.noreply.github.com>
2019-11-18 23:10:09 +0100

    Create main.h

---
+++ struct/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN.H
+#define MAIN.H
+
+
+// --- STRUCT ---
+typedef Vector vector
+struct Vector
+{
+    int x;
+    int y;
+    int z;
+};
+
+
+// --- PROTO ---
+void declareVec(struct Vector, struct Vector );
+void vecxvec(struct Vector, struct Vector);
+
+
+#endif // MAIN
