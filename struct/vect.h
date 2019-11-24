f0d04a127b2d800c05e4bb26df6dc647b5f4a1bd
JohnAtreides <43500984+JohnAtreides@users.noreply.github.com>
2019-11-25 00:08:39 +0100

    Add files via upload

---
+++ struct/vect.h
@@ -0,0 +1,20 @@
+#ifndef VECT
+#define VECT
+
+
+
+// --- STRUCT ---
+struct Vect
+{
+    int x;
+    int y;
+    int z;
+};
+
+
+// --- PROTO ---
+void declareVec(struct Vect *, struct Vect *);
+void vecxvec();
+
+
+#endif // VECT
