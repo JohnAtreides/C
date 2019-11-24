3f3c5bfe1595fc5ebe632267d4e37cf70fbef0c7
JohnAtreides <43500984+JohnAtreides@users.noreply.github.com>
2019-11-25 00:05:10 +0100

    Update main.c

--- struct/main.c
+++ struct/main.c
@@ -1,38 +1,38 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include<string.h>
-#include<stddef.h>
 
+#include "complex.h"
+#define FUNCCOMPLEXE 1
+#include "vect.h"
+#define FUNCVECT 2
+#define FUNCPLAY 3
 
-
-void declareVec(struct Vector U1, struct Vector V2)
-{
-    printf(" -U1-\n");
-    printf(" X1 ?\n");
-    scanf("%d", &U1->x);
-    printf(" Y1 ?\n");
-    scanf("%d", &U1->y);
-    printf(" Z1 ?\n");
-    scanf("%d", &U1->z);
-
-    printf(" -V2-\n");
-    printf(" X2 ?\n");
-    scanf("%d", &V2->x);
-    printf(" Y2 ?\n");
-    scanf("%d", &V2->y);
-    printf(" Z2 ?\n");
-    scanf("%d", &V2->y);
-}
-
-void vecxvec(struct Vector, struct Vector)
-{
-    Vector.x = 10;
-}
 
 int main()
 {
+    int mode=1;
+    printf(" Choose a mode : \n 1:Complexe \n 2:Vector \n 3:Player \n");
+    scanf("%d", &mode);
 
-    printf("Hello world!\n");
+    switch (mode)
+    {
+    case FUNCCOMPLEXE:
+        modeComplex();
+        break;
+
+    case FUNCVECT:
+        printf("FUNVECT");
+        struct Vect U, V;
+        declareVec(&U, &V);
+        break;
+
+    default:
+        printf("Choose smthg else :@ \n");
+        break;
+
+    }
 
     return 0;
+
 }
