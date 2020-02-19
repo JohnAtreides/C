82124dbe1a67b48892bb2704bc02d50ea909ec76
JohnAtreides <43500984+JohnAtreides@users.noreply.github.com>
2020-02-19 14:47:56 +0100

    Create main.c

---
+++ TTCheck/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "header.h"
+
+
+
+int main()
+{
+    timeDateCheck data;
+
+    dispTitle();
+
+    int lastTime=0;
+    data.lastTimeCheck=lastTime;
+    dataIn(data);
+
+
+
+
+
+    system("pause");
+    return 0;
+}
